b45f77554128a213e0f222807f98e87bc20f636e
daminal <drummersanfran@yahoo.com>
2016-08-29 15:51:40 -0400

    fixed #merge so all tests
    git commit -m fixed

--- lib/game.rb
+++ lib/game.rb
@@ -15,12 +15,18 @@ class Game
 	def clear_tribes
 		@tribes.clear
 	end
-  def merge(new_name)
-	#solution from: https://discussions.udacity.com/t/trouble-with-merge-in-game-class/164225
-    contestants = @tribes.map(&:members).flatten
-    clear_tribes
-    Tribe.new(name: new_name, members: contestants)
+		def merge(new_name)
+		contestants = @tribes[0].members + @tribes[1].members
+		clear_tribes
+		add_tribe(Tribe.new({name: new_name, members:contestants}))
+		return @tribes.first
 	end
+ #  def merge(new_name)
+	# #solution from: https://discussions.udacity.com/t/trouble-with-merge-in-game-class/164225
+ #    contestants = @tribes.map(&:members).flatten
+ #    clear_tribes
+ #    Tribe.new(name: new_name, members: contestants)
+	# end
 	def individual_immunity_challenge
 		@tribes[0].members.sample		
 	end
